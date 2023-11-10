e04775177f76f4fce47bbb092bf7a15d3b56210c
ReDMallard <marco@titori.it>
2023-11-10 04:36:08 +0100

    aggiugne controllo anche in disordine

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // --- crea numeri casuali e salva in un array (for)
 const pcNumbers = [];
 const userNumbers = [];
+let indovinate = 0;
 getRandom();
 
 
@@ -10,7 +11,7 @@ document.getElementById("numbers").innerHTML = pcNumbers;
 // --- nascondi numeri dopo 30 sec (timer setTimeout)
 setTimeout(eraseNumbers, 3000);
 // --- chiedi numeri ad utente e salva in un array (setTimeou)
-setTimeout(askNumbers, 3000)
+setTimeout(askNumbers, 3050)
 
 // --- confronta i due array e stampa risultato (if else )
 
@@ -24,9 +25,10 @@ setTimeout(askNumbers, 3000)
 
 // funzione random
 function getRandom() {
-    for(i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
         pcNumbers.push(Math.floor(Math.random() * 100) + 1);
     }
+    console.log(pcNumbers);
 
 }
 // funzione generica cancella innerelementbyid
@@ -35,14 +37,54 @@ function getRandom() {
 // };
 
 // funzione per cancellare innerHTML di numbers
-function eraseNumbers(){
+function eraseNumbers() {
     document.getElementById("numbers").innerHTML = "";
 }
 
 // funzione per chiedere 5 numeri all utente
 function askNumbers() {
-    for(i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
         userNumbers.push(parseInt(prompt("Che numero hai visto?")));
     };
+    
     console.log(userNumbers);
+    checkArrays(pcNumbers, userNumbers)
+
+}
+
+// funzione per verificare la corrispondenza tra i numeri random e i numeri utente(in generale tra i numeri di due array con la stessa lunghezza)
+//purtroppo vanno inseriti in ordine
+
+function checkArrays(firstArr, secondArr) {
+    
+
+    for (let i = 0; i < firstArr.length; i++) {
+        const element = secondArr[i];
+        console.log(element);
+
+        // if (element == firstArr[i]) {
+        if (firstArr.indexOf(element) > -1) {    
+            indovinate++;
+            
+            console.log("a");
+            console.log(indovinate);
+        }else{
+            
+            console.log("b");
+            console.log(indovinate);
+        }
+        
+        
+        console.log(indovinate);
+    }
+
+    if (indovinate == 5) {
+        console.log("hai vinto");
+        document.getElementById("numbers").innerHTML = `hai vinto`;
+
+    }else{
+        console.log("non hai vinto");
+        document.getElementById("numbers").innerHTML = `hai perso, hai indovinato solo ${indovinate} parole`;
+    }
+    
 }
