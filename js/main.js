6389fa846660ddc2494549835a57ec94a760f6a7
ReDMallard <marco@titori.it>
2023-11-10 02:37:37 +0100

    numeri scompaiono dopo tot tempo

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 const pcNumbers = [];
 const userNumbers = [];
 getRandom();
-document.getElementById("numbers").innerHTML = pcNumbers;
+
 
 // --- mostra numeri array su pagina (inner html)
+document.getElementById("numbers").innerHTML = pcNumbers;
 
-eraseId("numbers");
 // --- nascondi numeri dopo 30 sec (timer setTimeout)
+setTimeout(eraseNumbers, 3000);
 // --- chiedi numeri ad utente e salva in un array (setTimeou)
 // --- confronta i due array e stampa risultato (if else )
 
@@ -19,13 +20,21 @@ eraseId("numbers");
 
 // funzioni
 
+// funzione random
 function getRandom() {
     for(i = 0; i < 5; i++) {
         pcNumbers.push(Math.floor(Math.random() * 100) + 1);
     }
 
 }
-
-function eraseId(id) {
-    document.getElementById(id).innerHTML = "";
+// funzione generica cancella innerelementbyid
+// function eraseId(id) {
+//     document.getElementById(`"${id}"`).innerHTML = "";
+// };
+function eraseNumbers(){
+    document.getElementById("numbers").innerHTML = "";
 }
+
+
+
+
