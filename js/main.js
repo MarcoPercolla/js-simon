e8db51878687706a3a8c805242014c1e4b12a5ac
ReDMallard <marco@titori.it>
2023-11-10 02:14:02 +0100

    add 5 random numbers on page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 // --- crea numeri casuali e salva in un array (for)
+const pcNumbers = [];
+const userNumbers = [];
+getRandom();
+document.getElementById("numbers").innerHTML = pcNumbers;
+
 // --- mostra numeri array su pagina (inner html)
 // --- nascondi numeri dopo 30 sec (timer setTimeout)
 // --- chiedi numeri ad utente e salva in un array (setTimeou)
 // --- confronta i due array e stampa risultato (if else )
+
+
+
+
+
+
+
+// funzioni
+
+function getRandom() {
+    for(i = 0; i < 5; i++) {
+        pcNumbers.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+}
